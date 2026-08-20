1e186bc2bfe5a20ce900c8da8d966d85118db349
nix <you@example.com>
2026-08-20 20:30:58 +0300

    завершение таск 3. попытка запуска 4.

--- tasks/task3/booking-subgraph/grpcClient.js
+++ tasks/task3/booking-subgraph/grpcClient.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const BOOKING_SERVICE_GRPC_SOCKET = process.env.BOOKING_SERVICE_GRPC_SOCKET || 'booking-service:9090';
-
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PROTO_PATH = path.join(__dirname, 'booking.proto');
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
