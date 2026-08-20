9c89998ad050916cf63876cbc8adc229649f8904
nix <you@example.com>
2026-08-20 20:37:48 +0300

    завершение таск 3. попытка запуска 5.

--- tasks/task3/booking-subgraph/grpcClient.js
+++ tasks/task3/booking-subgraph/grpcClient.js
@@ -2,6 +2,7 @@ import grpc from '@grpc/grpc-js';
 import protoLoader from '@grpc/proto-loader';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { promisify } from 'util';
 
 const BOOKING_SERVICE_GRPC_SOCKET = process.env.BOOKING_SERVICE_GRPC_SOCKET || 'booking-service:9090';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -17,7 +18,6 @@ const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 const bookingProto = protoDescriptor.booking;
 
 const client = new bookingProto.BookingService(BOOKING_SERVICE_GRPC_SOCKET, grpc.credentials.createInsecure());
-const { promisify } = require('util');
 const listBookings = promisify(client.listBookings).bind(client);
 
 export { listBookings };
